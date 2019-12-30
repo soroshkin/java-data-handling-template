a1afb10f9aea0b6a095b9f80e28dc5c80ae987fc
Soroshkin Anton <soroshkin_a_m@mail.ru>
2019-12-30 18:44:30 +0300

    Update SimpleBigNumbersService class via "Sieve of Eratosthenes" method

--- src/main/java/com/epam/izh/rd/online/service/SimpleBigNumbersService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleBigNumbersService.java
@@ -27,25 +27,34 @@ public class SimpleBigNumbersService implements BigNumbersService {
      */
     @Override
     public BigInteger getPrimaryNumber(int range) {
-        BigInteger lastPrimaryNumberFound = BigInteger.valueOf(2);
-        int primaryNumberIndex = 0;
-        boolean isPrimeFound = true;
+        BigInteger numberOfElementsToCheck = BigInteger.valueOf((long) (50 + 1.11182 * range * Math.log(1.20661 * range + 0.00435)));
+        if (numberOfElementsToCheck.compareTo(BigInteger.valueOf(Integer.MAX_VALUE).subtract(BigInteger.ONE)) > 0) {
+            System.out.println("Value \"range\" is out of range, try to decrease it");
+            return BigInteger.ZERO;
+        }
 
-        while (primaryNumberIndex != range) {
-            lastPrimaryNumberFound = lastPrimaryNumberFound.add(BigInteger.ONE);
-            for (BigInteger i = BigInteger.valueOf(2); i.compareTo(lastPrimaryNumberFound.subtract(BigInteger.ONE)) != 0; i = i.add(BigInteger.ONE)) {
-                if (lastPrimaryNumberFound.remainder(i).compareTo(BigInteger.ZERO) == 0) {
-                    isPrimeFound = false;
-                    break;
-                } else {
-                    isPrimeFound = true;
-                }
-            }
-            if (isPrimeFound) {
-                primaryNumberIndex++;
+        boolean[] primaryNumbers = new boolean[numberOfElementsToCheck.intValue()];
+        Arrays.fill(primaryNumbers, true);
+        primaryNumbers[0] = false;
+        primaryNumbers[1] = false;
+
+        for (int i = 2; numberOfElementsToCheck.compareTo(BigInteger.valueOf(i)) > 0; i++) {
+            for (int j = 2; numberOfElementsToCheck.compareTo(BigInteger.valueOf(j * i)) > 0; j++) {
+                primaryNumbers[j * i] = false;
             }
         }
-        return lastPrimaryNumberFound;
+
+        int primaryNumberIndex = -1;
+        BigInteger primaryNumberFound = BigInteger.ZERO;
+        for (int i = 0; i < primaryNumbers.length; i++) {
+            if (primaryNumbers[i]) {
+                primaryNumberIndex++;
+                if (primaryNumberIndex == range) {
+                   return BigInteger.valueOf(i);
+                }
+            }
+        }
+        return primaryNumberFound;
     }
 
     public static void main(String[] args) {
