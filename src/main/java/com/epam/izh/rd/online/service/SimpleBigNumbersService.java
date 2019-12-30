d2d8250e21c4c97b31ef321337a4a8ba3316d421
Soroshkin Anton <soroshkin_a_m@mail.ru>
2019-12-30 18:27:44 +0300

    Update SimpleBigNumbersService class via linear search

--- src/main/java/com/epam/izh/rd/online/service/SimpleBigNumbersService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleBigNumbersService.java
@@ -2,18 +2,21 @@ package com.epam.izh.rd.online.service;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.MathContext;
+import java.util.*;
 
 public class SimpleBigNumbersService implements BigNumbersService {
 
     /**
      * Метод делит первое число на второе с заданной точностью
      * Например 1/3 с точностью 2 = 0.33
+     *
      * @param range точность
      * @return результат
      */
     @Override
     public BigDecimal getPrecisionNumber(int a, int b, int range) {
-        return null;
+        return new BigDecimal(a / (b * 1.0), new MathContext(range));
     }
 
     /**
@@ -24,6 +27,27 @@ public class SimpleBigNumbersService implements BigNumbersService {
      */
     @Override
     public BigInteger getPrimaryNumber(int range) {
-        return null;
+        BigInteger lastPrimaryNumberFound = BigInteger.valueOf(2);
+        int primaryNumberIndex = 0;
+        boolean isPrimeFound = true;
+
+        while (primaryNumberIndex != range) {
+            lastPrimaryNumberFound = lastPrimaryNumberFound.add(BigInteger.ONE);
+            for (BigInteger i = BigInteger.valueOf(2); i.compareTo(lastPrimaryNumberFound.subtract(BigInteger.ONE)) != 0; i = i.add(BigInteger.ONE)) {
+                if (lastPrimaryNumberFound.remainder(i).compareTo(BigInteger.ZERO) == 0) {
+                    isPrimeFound = false;
+                    break;
+                } else {
+                    isPrimeFound = true;
+                }
+            }
+            if (isPrimeFound) {
+                primaryNumberIndex++;
+            }
+        }
+        return lastPrimaryNumberFound;
+    }
+
+    public static void main(String[] args) {
     }
 }
